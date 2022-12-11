2c24efd03618d0d8a7cb412e21a97d7594575d16
Juan Calvopina <juan.calvopina@gmail.com>
2022-12-11 02:36:42 -0500

    Upgrade to Spring Boot 3 | Update resources folder for tests

--- src/test/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `sample-envers`.`env_person`(first_name, last_name)
-VALUES ('Juan', 'Calvopina');
-
-INSERT INTO `sample-envers`.`env_product`(description, `name`, quantity_per_unit, unit_price)
-VALUES ('Apple product', 'macbook pro', 1, 2500.0);
